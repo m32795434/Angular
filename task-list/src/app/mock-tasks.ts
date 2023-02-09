8e9296be138b10e4e73799bd10c1c311bf319564
Manuel Augusto Bravard <manubravard@gmail.com>
2023-02-09 10:37:55 -0300

    mock created & Task interface

---
+++ task-list/src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from './task';
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Terminar primer módulo de Angular',
+        day: 'Agosto 15',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Terminar Segundo módulo de Angular',
+        day: 'Septiembre 15',
+        reminder: false
+    },
+    {
+        id: 3,
+        text: 'Terminar primer módulo de Typescript',
+        day: 'Octubre 15',
+        reminder: true
+    },
+    {
+        id: 4,
+        text: 'Terminar segundo módulo de Typescript',
+        day: 'Noviembre 15',
+        reminder: false
+    },
+];
