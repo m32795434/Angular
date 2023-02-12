b65cee499de61abd3ac734ea656d97ad5f4d2977
Manuel Augusto Bravard <manubravard@gmail.com>
2023-02-12 00:41:22 -0300

    get method

--- task-list/src/app/app.module.ts
+++ task-list/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -18,7 +19,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- task-list/src/app/services/task.service.ts
+++ task-list/src/app/services/task.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { TASKS } from '../mock-tasks';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+// import { TASKS } from '../mock-tasks';
 import { Task } from '../task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+  constructor(
+    private http: HttpClient
+  ) { }
 
   getTasks(): Observable<Task[]> {
-    return of(TASKS);
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
