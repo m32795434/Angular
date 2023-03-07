0aac27f8716424572eaa0f45b7a21d5c5958f169
Manuel Augusto Bravard <manubravard@gmail.com>
2023-03-07 13:01:03 -0300

    stand alone ngx component test

--- 01-bases/src/app/popOverAutoClose.ts
+++ 01-bases/src/app/popOverAutoClose.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
+    //StandAlone or not, you can instantiate various times this component, and pass them some values
     selector: 'ngbd-popover-autoclose',
     standalone: true,
     imports: [NgbPopoverModule],
